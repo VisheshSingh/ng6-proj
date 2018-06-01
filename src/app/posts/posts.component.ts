a96fac81729338345b6560bbdf46f37c0527d46a
Vishesh <visheshsingh16@gmail.com>
2018-06-01 11:20:05 -0500

    fetching posts from jsonplaceholder api

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../data.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  selector: "app-posts",
+  templateUrl: "./posts.component.html",
+  styleUrls: ["./posts.component.scss"]
 })
 export class PostsComponent implements OnInit {
-
-  constructor() { }
+  posts: Object;
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
+    this.dataService.getPosts().subscribe(post => {
+      this.posts = post;
+    });
   }
-
 }
